74fa1e1e8e27059a2381ffc4ac5437426182fd12
DisonUyoga <disonobudho233@gmail.com>
2023-10-16 02:31:01 -0700

    'tt'

--- src/pages/loginApi/getCred.js
+++ src/pages/loginApi/getCred.js
@@ -1,44 +1,24 @@
 import axios from 'axios'
 
 export async function loginUser(creds){
-    const res = await fetch("http://localhost:8000/api/v1/token/login",{
-        method:"post",
-        headers:{
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(creds)
-    
-    })
-    const data= await res.json()
-    if(!res.ok){
-        throw{
-            message: "Invalid credentials",
-            statusText: res.statusText,
-            status:res.status
-        }
-        
+    try{
+        const res = await axios.post("/api/v1/token/login",creds)
+        return res.data
+    }catch(err){
+        return err.message
     }
-    return data
+    
+   
 }
 
 export async function signupUsers(creds){
-    const res = await fetch("http://localhost:8000/api/v1/users/",{
-        method:"post",
-        headers:{
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(creds)
-    })
+    try{
+    const res = await axios.post("/api/v1/users/",creds)
+    return res.data
+    }catch(err){
+     return err.message
+     }
 
-    const data = await res.json()
-    if(!res.ok){
-        throw{
-            message: "Something went wrong please try again",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    return data
 }
 
 export async function searchData(item){
